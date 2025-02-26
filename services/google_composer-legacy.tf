a066a44b69558e04e8024be149a569f47e777932
TejaChittamuri <111578142+Teja-Chittamuri@users.noreply.github.com>
2025-02-27 01:23:22 +0530

    Update google_composer-legacy.tf

--- services/google_composer-legacy.tf
+++ services/google_composer-legacy.tf
@@ -1,35 +1,37 @@
-provider "google" {
-  project = "your-gcp-project-id"
-  region  = "us-central1"
-}
-
-resource "google_composer_environment" "composer_legacy" {
-  name   = "my-composer-env"
+resource "google_composer_environment" "example" {
+  name   = "example-composer-env"
   region = "us-central1"
 
   config {
-    node_count = 3
-
     software_config {
-      image_version = "composer-1.19.16-airflow-1.10.15"
-
-      # Override Airflow configurations
       airflow_config_overrides = {
-         core.lazy_load_plugins        = "False"
-         core.load_default_connections = "False"
-         secrets.backend               = "airflow.providers.google.cloud.secrets.secret_manager.CloudSecretManagerBackend"
-         secrets.backend_kwargs       = <<EOT
-          {
-            "connections_prefix": "airflow-connections",
-            "variables_prefix": "airflow-variables",
-            "gcp_project_id": "your-gcp-project-id"
-          }
+        webserver-dag_default_view         = "graph"
+        core-dagbag_import_timeout         = "240"
+        core-dags_are_paused_at_creation   = "True"
+        scheduler-catchup_by_default       = "False"
+        webserver-scheduler_zombie_task_threshold = "50"
+        webserver-worker_refresh_interval  = "30"
+        webserver-rbac_user_registration_role = "Viewer"
+        secrets-backend = "airflow.providers.google.cloud.secrets.secret_manager.CloudSecretManagerBackend"
+        secrets-backend_kwargs = <<EOT
+        {
+          "connections_prefix": "${coalesce(var.secret_conn_prefix, "airflow-connections-${var.cmpsr_name}-${each.key}")}",
+          "variables_prefix": "${coalesce(var.secret_var_prefix, "airflow-variables-${var.cmpsr_name}-${each.key}")}"
+        }
         EOT
       }
-    }
 
-    node_config {
-      service_account = "your-service-account@your-gcp-project-id.iam.gserviceaccount.com"
+      image_version = try(var.env_overrides[each.key].image_version, var.image_version)
     }
   }
+
+  lifecycle {
+    ignore_changes = [
+      config[0].software_config[0].airflow_config_overrides,
+      config[0].software_config[0].env_variables,
+      config[0].software_config[0].pypi_packages
+    ]
+  }
+
+  depends_on = [google_project_iam_member.composer2_svcagent_iam]
 }
